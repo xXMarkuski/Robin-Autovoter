830261bc3fe0f6407c17f5b03ffda98e77461fd4
xXMarkuski <xxmodplayer@gmail.com>
2016-04-02 09:35:59 +0200

    simplify

--- robinautovoter.user.js
+++ robinautovoter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Autovoter
 // @namespace    http://jerl.im
-// @version      1.92
+// @version      2.0
 // @description  Autovotes via text on /r/robin
 // @author       /u/keythkatz and /u/xxmarkuski
 // @match        https://www.reddit.com/robin*
@@ -9,10 +9,6 @@
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
-var PREFIX = "......................";
-
-var COMMANDO_SPAMMER = PREFIX + "I should be the one";
-var COMMANDO_SOMEONEELETAKEOVER = PREFIX + "Reloading, ";
 var messages = document.getElementsByClassName("robin-message");
 
 function sendMessage(message){
@@ -21,22 +17,16 @@ function sendMessage(message){
 }
 
 var config = /** @type {MutationObserverInit} */ ({ attributes: true, childList: true, characterData: true });
-new MutationObserver(function callback(param1, param2){if(messages.length == 4){init();}}).observe(document.getElementById("robinChatMessageList"), config);
+new MutationObserver(function callback(param1, param2){
+    var shouldInit = false;
+        if(messages[messages.length-1].getElementsByClassName("robin-message--message")[0].innerText == "connected!" && messages[messages.length-1].getElementsByClassName("robin-message--from")[0].innerText == "[robin]"){
+            shouldInit = true;
+    }
+    if(shouldInit){
+        init();
+    }
+}).observe(document.getElementById("robinChatMessageList"), config);
 
 function init(){
     sendMessage("/vote grow");
-    var shouldMessage = true;
-    var i;
-    for (i = 0; i < messages.length; i++) {
-        if(messages[i].getElementsByClassName("robin-message--message")[0].innerText == COMMANDO_SPAMMER){
-            shouldMessage = false;
-            break;
-        }
-    }
-    if(shouldMessage){
-        sendMessage("");
-    }
-    setTimeout(function(){
-        window.location.reload();
-    }, 300000);
 }
